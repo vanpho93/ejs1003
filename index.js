938ffecb1a1bb1fe98044cb84df29e32c562188d
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-27 17:56:23 +0700

    html

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('<img src="g.png"/>');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 app.listen(3000, () => console.log('Server started!'));
