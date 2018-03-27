c433ef654b61e4dc8ce60b552c23ac48ee2bfbb3
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-27 18:59:02 +0700

    connect ejs and js

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('main');
+    const arrWords = [
+        { _id: 'abcd1', en: 'One', vn: 'Mot', isMemorized: true },
+        { _id: 'abcd2', en: 'Two', vn: 'Hai', isMemorized: false },
+        { _id: 'abcd3', en: 'Three', vn: 'Ba', isMemorized: false },
+        { _id: 'abcd4', en: 'Five', vn: 'Bon', isMemorized: true },
+    ];
+    res.render('main', { troiMua: false, arrWords });
 });
 
 app.listen(3000, () => console.log('Server started!'));
